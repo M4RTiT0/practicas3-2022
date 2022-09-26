ecf5513def5a9f2320c403701797a3a2a1c49de0
M4RTiT0 <martotamborini8@gmail.com>
2022-09-26 16:20:50 -0300

    sdf

---
+++ prueba/src/components/FormCliente.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1> Probando Componentes </h1>
+    <input type="text">
+    <button>Aceptar</button>
+</template>
+<script>
+/* eslint-disable */
+export default {
+    setup() {},
+}
+</script>
---
+++ prueba/src/views/Cliente.vue
@@ -0,0 +1,13 @@
+<template>
+    <FormCliente />
+</template>
+<script>
+import FormCliente from "../components/FormCliente.vue"
+export default {
+    name: 'Cliente',
+    components:{
+        FormCliente
+    },
+    setup() {},
+}
+</script>
