8a2170e5e834aaacf3820ac26b8f052d4a39bc3e
M4RTiT0 <martotamborini8@gmail.com>
2022-09-26 17:47:01 -0300

    ertyhjk

--- prueba/src/components/FormCliente.vue
+++ prueba/src/components/FormCliente.vue
@@ -1,11 +1,35 @@
 <template>
-    <h1> Probando Componentes </h1>
-    <input type="text">
-    <button>Aceptar</button>
+    <div class="row">
+        <div class="col-lg-4">
+            <h2> Probando Componentes </h2>
+            <div class="mb-3">
+                <label class="form-label">Nombre</label>
+                <input v-model="nombre" class="form-control" type="text">
+            </div>
+            <button @click="mostrar()" class="btn btn-success">Aceptar</button>
+        </div>
+    </div>
 </template>
 <script>
 /* eslint-disable */
+import { ref } from 'vue'
 export default {
-    setup() {},
+    name:'FormCliente',
+    setup() {
+
+        //sector de variables
+        let nombre = ref('')
+
+        //sector de funciones
+        function mostrar(){
+            alert(nombre.value)
+        }
+
+        return{
+            nombre,
+            mostrar
+        }
+
+    },
 }
 </script>
