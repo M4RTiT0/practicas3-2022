7923f78f940f673dbba47135015635920f7e9551
M4RTiT0 <martotamborini8@gmail.com>
2022-10-03 20:38:12 -0300

    fgh

--- prueba/src/main.js
+++ prueba/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootswatch/dist/quartz/bootstrap.css'
+import 'bootswatch/dist/slate/bootstrap.css'
 
 createApp(App).use(router).mount('#app')
